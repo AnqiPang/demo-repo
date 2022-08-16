6502022c20871d64f90d785c5bde7717f2da61a5
Anqi Pang <anqipans@amazon.com>
2022-08-15 17:21:12 -0700

    tag v0.3.3

--- src/HelloWorld.go
+++ src/HelloWorld.go
@@ -1,4 +1,4 @@
-//usr/bin/env go run "$0" "$@"; exit "$?"
+// usr/bin/env go run "$0" "$@"; exit "$?"
 package main
 
 import (
@@ -7,6 +7,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, v0.3.2!")
+	fmt.Println("Hello, v0.3.3!")
 	os.Exit(42)
 }
